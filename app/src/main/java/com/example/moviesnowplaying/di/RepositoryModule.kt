055cb823c049f4a62da79c51b5e7370b18911108
Ace Atienza <aceatienza@users.noreply.github.com>
2020-07-13 17:54:28 +0200

    fix: specify MovieRepository type
    note: equivalent to Dagger @Provides then using an implementation

--- app/src/main/java/com/example/moviesnowplaying/di/RepositoryModule.kt
+++ app/src/main/java/com/example/moviesnowplaying/di/RepositoryModule.kt
@@ -1,9 +1,10 @@
 package com.example.moviesnowplaying.di
 
+import com.example.moviesnowplaying.data.repositories.MovieRepository
 import com.example.moviesnowplaying.data.repositories.MovieRepositoryImpl
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val repositoryModule: Module = module {
-    factory { MovieRepositoryImpl(get()) }
+    factory<MovieRepository> { MovieRepositoryImpl(get()) }
 }
