34da39b7e7ede31acce3647358dce3bf833c6a0e
Ace Atienza <aceatienza@users.noreply.github.com>
2020-07-13 14:48:43 +0200

    feat: update Fragment and ViewModel for movie list

--- app/src/main/java/com/example/moviesnowplaying/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/moviesnowplaying/ui/main/MainFragment.kt
@@ -1,12 +1,14 @@
 package com.example.moviesnowplaying.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.moviesnowplaying.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.moviesnowplaying.databinding.MainFragmentBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
 
@@ -14,17 +16,38 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private lateinit var moviesAdapter: MoviesAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    /**
+     *     note: the correct import is "org.koin.androidx.viewmodel.ext.android.viewModel",
+     *     not org.koin.androidx.viewmodel.compat.ScopeCompat.viewModel, or import org.koin.androidx.viewmodel.compat.ViewModelCompat.viewModel
+     */
+    val mainViewModel: MainViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = MainFragmentBinding.inflate(inflater, container, false)
+
+        moviesAdapter = MoviesAdapter()
+        binding.rvMoviesShort.adapter = moviesAdapter
+        binding.rvMoviesShort.layoutManager = LinearLayoutManager(context)
+
+        mainViewModel.getMovies()
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        mainViewModel.movies.observe(viewLifecycleOwner, Observer {
+            if (it.isNullOrEmpty())
+                moviesAdapter.update(emptyList())
+            else
+                moviesAdapter.update(it)
+        })
+    }
 }
--- app/src/main/java/com/example/moviesnowplaying/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/moviesnowplaying/ui/main/MainViewModel.kt
@@ -1,7 +1,28 @@
 package com.example.moviesnowplaying.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.moviesnowplaying.data.models.MovieShort
+import com.example.moviesnowplaying.domain.MoviesNowPlayingUseCase
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(
+    private val moviesNowPlayingUseCase: MoviesNowPlayingUseCase
+) : ViewModel() {
+
+    private val _movies = MutableLiveData<List<MovieShort>?>()
+    val movies: LiveData<List<MovieShort>?> get() = _movies
+
+    fun getMovies() {
+        viewModelScope.launch {
+            moviesNowPlayingUseCase.execute(MoviesNowPlayingUseCase.Param).collect {
+                val results = if (it is MoviesNowPlayingUseCase.Result.Success) it.movies else null
+                _movies.postValue(results)
+            }
+        }
+    }
+
 }
