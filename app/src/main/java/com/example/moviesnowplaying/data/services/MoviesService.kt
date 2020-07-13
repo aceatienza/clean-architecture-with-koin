b705a6a4891ee82237e321e2177fb42a4d374d30
Ace Atienza <aceatienza@users.noreply.github.com>
2020-07-13 17:50:36 +0200

    fix: update path and replace header with query param for api_key

--- app/src/main/java/com/example/moviesnowplaying/data/services/MoviesService.kt
+++ app/src/main/java/com/example/moviesnowplaying/data/services/MoviesService.kt
@@ -1,17 +1,19 @@
 package com.example.moviesnowplaying.data.services
 
+import com.example.moviesnowplaying.BuildConfig
+import com.example.moviesnowplaying.TMDB_QUERY_PARAM_API_KEY
 import com.example.moviesnowplaying.network.response.NowPlayingResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 
 interface MoviesService {
 
     @Headers(
-        AuthType.BEARER,
         ContentType.JSON
     )
-    @GET("/movie/now_playing")
-    suspend fun getNowPlaying(): Response<NowPlayingResponse>
+    @GET("movie/now_playing")
+    suspend fun getNowPlaying(@Query(TMDB_QUERY_PARAM_API_KEY) apiKey: String = BuildConfig.TMDB_API_KEY): Response<NowPlayingResponse>
 }
